404f2aa7e6dadd57bd84be6322dff0148a2e85a2
Dnlbellfield <dnlbellfield@gmail.com>
2022-02-24 23:42:38 -0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,133 @@
+document.getElementById('kim-usman').onclick = kimUsman
+document.getElementById('caleb-alina').onclick = calebAlina
+document.getElementById('gino-jasmine').onclick = ginoJasmine
+document.getElementById('memphis-hamza').onclick = memphisHamza
+document.getElementById('mike-ximena').onclick = mikeXimena
+document.getElementById('ella-johnny').onclick = ellaJohnny
+document.getElementById('ben-mahogany').onclick = benMahogany
+
+
+function kimUsman() {
+  document.getElementsByClassName('img')[0].style.backgroundImage = "url('img/kim-usman.jpeg')";
+  document.getElementById('video').src = 'video/kim-usman-edit.mov';
+  
+  const coupleAgeLocation = document.getElementById('couple-age-location');
+  coupleAgeLocation.style.backgroundColor = 'rgba(236, 159, 58, 1)';
+  coupleAgeLocation.style.visibility = 'visible';
+  coupleAgeLocation.innerText = 'Kim, 50 (San Diego, Calif.), and Usman  Sojaboy,  32 (Sokoto, Nigeria)';
+
+  const coupleStory = document.getElementById('story-text');
+  coupleStory.style.backgroundColor = 'rgba(236, 159, 58, 1)';
+  coupleStory.style.background = 'rgba(236, 159, 58, 1)';
+  coupleStory.style.visibility = 'visible';
+  coupleStory.innerText = 'A year ago, 50-year-old divorcee Kim reached out to Nigerian musician Usman, a.k.a. Soja Boy, on social media as a fan of his music. But after months of calls, texts, and video chats the two formed a connection, and Kim found herself head over heels in love, graduating from  fan  to  potential girlfriend.  Considering his divorce from another older American woman that he also met online, Usman has hesitations about committing to Kim until they meet in person, so he\'s invited her to Tanzania, where he plans to shoot a music video. Kim is risking everything for love and heading to Tanzania in hopes that Usman will realize she is nothing like his ex-wife and decide to commit to taking their relationship to the next level';
+}
+
+function calebAlina() {
+  document.getElementsByClassName('img')[0].style.backgroundImage = "url('img/caleb-alina.jpeg')";
+  document.getElementById('video').src = 'video/caleb-alina-edit.mov';
+
+  const coupleAgeLocation = document.getElementById('couple-age-location');
+  coupleAgeLocation.style.backgroundColor = 'rgba(236, 159, 58, 1)';
+  coupleAgeLocation.style.visibility = 'visible';
+  coupleAgeLocation.innerText = 'Caleb, 28 (Chandler, Ariz.), and Alina, 27 (Saint Petersburg, Russia)';
+
+  const coupleStory = document.getElementById('story-text');
+  coupleStory.style.backgroundColor = 'rgba(236, 159, 58, 1)';
+  coupleStory.style.background = 'rgba(236, 159, 58, 1)';
+  coupleStory.style.visibility = 'visible';
+  coupleStory.innerText = 'Caleb and Alina connected and developed a friendship on social media as teens, but they lost touch for 13 years until they serendipitously matched on a dating app when Caleb was planning a trip to Alina\'s home country of Russia. Those plans fell through, but their newly rekindled virtual friendship blossomed into something much deeper. Now they\'re finally ready to meet for the first time to see if their emotional connection is deep enough to overcome their physical differences. Alina is a little person, and her condition prevents her from walking and standing for long periods of time, so she frequently uses on a wheelchair, making free-spirit Caleb wonder if he\'ll be able to adjust to her needs. Caleb also has to win over Alina\'s roommate and protective best friend Elijah, who joins them on their introductory rendezvous in Turkey.';
+  
+}
+
+function ginoJasmine() {
+  document.getElementsByClassName('img')[0].style.backgroundImage = "url('img/gino-jasmine.jpeg')";
+  document.getElementById('video').src = 'video/gino-jamsmine-edit.mov';
+
+  const coupleAgeLocation = document.getElementById('couple-age-location');
+  coupleAgeLocation.style.backgroundColor = 'rgba(236, 159, 58, 1)';
+  coupleAgeLocation.style.visibility = 'visible';
+  coupleAgeLocation.innerText = 'Gino, 51 (Canton, Mich.), and Jasmine, 34 (Panama City, Panama)';
+
+  const coupleStory = document.getElementById('story-text');
+  coupleStory.style.backgroundColor = 'rgba(236, 159, 58, 1)';
+  coupleStory.style.background = 'rgba(236, 159, 58, 1)';
+  coupleStory.style.visibility = 'visible';
+  coupleStory.innerText = 'Gino, a 51-year-old automotive engineer, is leaving for Panama to meet the love of his life, Jasmine, a 34-year-old American literature teacher. After divorcing his Brazilian wife of seven years, Gino began to lose hope of ever starting his own family, until he met Jasmine on an international dating site and fell in love with her personality, brains, and beauty. He is hoping to propose and get Jasmine pregnant by the end of his trip so that he can have the family he\'s always wanted. Will Jasmine be the one that finally sweeps Gino off his feet, or will her jealous side put a damper on their future together?';
+  
+  
+}
+
+
+function memphisHamza() {
+  document.getElementsByClassName('img')[0].style.backgroundImage = "url('img/memphis-hamza.jpeg')";
+  document.getElementById('video').src = 'video/memphis-hamza-edit.mov';
+  
+  const coupleAgeLocation = document.getElementById('couple-age-location');
+  coupleAgeLocation.style.backgroundColor = 'rgba(236, 159, 58, 1)';
+  coupleAgeLocation.style.visibility = 'visible';
+  coupleAgeLocation.innerText = 'Memphis, 34 (Muskegon, Mich.), and Hamza, 28 (Kairouan, Tunisia)';
+
+  const coupleStory = document.getElementById('story-text');
+  coupleStory.style.backgroundColor = 'rgba(236, 159, 58, 1)';
+  coupleStory.style.background = 'rgba(236, 159, 58, 1)';
+  coupleStory.style.visibility = 'visible';
+  coupleStory.innerText = 'Memphis, a divorced single mom of two, met Hamza, a 28-year-old man from Tunisia, online eight months ago. Having had an unstable childhood, followed by a tumultuous dating history, Memphis has been searching for a consistent and reliable partner with whom to complete her family — and she found that in Hamza. After a few months of daily texts and video chats, Hamza proposed and Memphis said yes! Despite a significant language barrier and the skepticism of her friends and family, she is packing her bags and traveling to Tunisia with plans to get married on this trip. But does Memphis really know her fiancé?';
+}
+
+
+function mikeXimena() {
+  document.getElementsByClassName('img')[0].style.backgroundImage = "url('img/mike-ximena.jpeg')";
+  document.getElementById('video').src = 'video/mike-ximena-edit.mov';
+  
+  const coupleAgeLocation = document.getElementById('couple-age-location');
+  coupleAgeLocation.style.backgroundColor = 'rgba(236, 159, 58, 1)';
+  coupleAgeLocation.style.visibility = 'visible';
+  coupleAgeLocation.innerText = 'Mike, 34 (Thiells, N.Y.), and Ximena, 24 (Pereira, Colombia)';
+
+  const coupleStory = document.getElementById('story-text');
+  coupleStory.style.backgroundColor = 'rgba(236, 159, 58, 1)';
+  coupleStory.style.background = 'rgba(236, 159, 58, 1)';
+  coupleStory.style.visibility = 'visible';
+  coupleStory.innerText = 'Despite his best efforts at finding love, Mike had been single for most of his life — until he matched with Ximena, a 24-year-old Colombian single mother of two, a year ago on an international dating app. Ximena speaks no English and Mike speaks only a tiny bit of Spanish, but that hasn\'t kept them from falling in love with the help of a translator app. They\'re already talking about marriage and kids, so Mike is traveling to Colombia with plans to propose and bring Ximena and her sons to the U.S. Has Mike finally found true love after years of searching, or is Ximena more in love with the gifts and money he sends her than with Mike himself?';
+}
+
+function ellaJohnny() {
+  document.getElementsByClassName('img')[0].style.backgroundImage = "url('img/ella-johnny.jpeg')";
+  document.getElementById('video').src = 'video/ella-johnny-edit.mov';
+  
+  const coupleAgeLocation = document.getElementById('couple-age-location');
+  coupleAgeLocation.style.backgroundColor = 'rgba(236, 159, 58, 1)';
+  coupleAgeLocation.style.visibility = 'visible';
+  coupleAgeLocation.innerText = 'Ella, 29 (Idaho Falls, Idaho), and Johnny, 34 (Jinan, China)';
+
+  const coupleStory = document.getElementById('story-text');
+  coupleStory.style.backgroundColor = 'rgba(236, 159, 58, 1)';
+  coupleStory.style.background = 'rgba(236, 159, 58, 1)';
+  coupleStory.style.visibility = 'visible';
+  coupleStory.innerText = 'Ella, a country girl from Idaho with a self-proclaimed obsession with Asian culture, met Johnny — her  Asian prince  — on a social media platform exclusively for Asian men and white women. Once they started video chatting, they realized they\'d found their soul mates in one another. The pandemic has made it difficult for them to meet in person, and China\'s borders remain closed, but after a year and a half and a number of false starts, they\'re finally preparing for Johnny to visit Ella in her hometown and get engaged. Despite having video chatted  intimately  over the past few months, Ella\'s history with rejection from past romantic interests has her worried about what it will be like when she and Johnny are together in person. Still, they\'re both ready to prove that their love can conquer all, if they can overcome family skepticism and pandemic barriers.';
+}
+
+function benMahogany() {
+  document.getElementsByClassName('img')[0].style.backgroundImage = "url('img/ben-mahogany.jpeg')";
+  document.getElementById('video').src = 'video/ben-mahogany-edit.mov';
+  
+  const coupleAgeLocation = document.getElementById('couple-age-location');
+  coupleAgeLocation.style.backgroundColor = 'rgba(236, 159, 58, 1)';
+  coupleAgeLocation.style.visibility = 'visible';
+  coupleAgeLocation.innerText = 'Ben, 52 (Fraser, Mich.), and Mahogany, 24 (San Bartolo, Peru)';
+
+  const coupleStory = document.getElementById('story-text');
+  coupleStory.style.backgroundColor = 'rgba(236, 159, 58, 1)';
+  coupleStory.style.background = 'rgba(236, 159, 58, 1)';
+  coupleStory.style.visibility = 'visible';
+  coupleStory.innerText = 'When Ben, a former pastor in a strict, conservative religious sect, broke free from confinement and his marriage, he struggled to find his way forward. After a bout of homelessness, he eventually found work as a model and caught Mahogany\'s eye after being featured in a fitness magazine. Her fateful DM led to an intense texting relationship. Bonded by their similar religious views, they fell in love and began making plans to meet in person, despite a 28-year age difference. Ben\'s friends and family see countless red flags, from the significant age gap between them to the fact that they have never video chatted, but Ben believes that God has brought them together and this is the path he is meant to follow. Is their relationship divine will, or is Ben blinded to the possibility that Mahogany may not be the person she said she is?';
+}
+
+// id="kim-usman"
+// id="caleb-alina"
+// id="gino-jasmine"
+// id="memphis-hamza"
+// id="mike-ximena"
+// id="ella-johnny"
+// id="ben-mahogany"
